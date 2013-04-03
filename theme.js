5bf5f67dacf3261ce5471f0646cc3668fa706384
Friedrich Lindenberg <friedrich@pudo.org>
2013-04-03 20:09:53 +0200

    Some fixes.

--- theme.js
+++ theme.js
@@ -3,7 +3,7 @@
     // SPIEGEL ONLINE Theme
     // -------------
 
-    Datawrapper.Themes.SpOn = $.extend(true, {}, Datawrapper.Themes.Base, {
+    Datawrapper.Themes.Spon = $.extend(true, {}, Datawrapper.Themes.Base, {
 
         colors: {
             palette: ['#a030415', '#06234c', '#014d01', '#f9830e', '#5e3310', '#4fb4c'],
