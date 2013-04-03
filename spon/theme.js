193a6ba6bca1580856abdde204c375be910f0ab1
Friedrich Lindenberg <friedrich@pudo.org>
2013-04-03 19:52:12 +0200

    Spon Theme basics.

---
+++ spon/theme.js
@@ -0,0 +1,24 @@
+(function(){
+
+    // SPIEGEL ONLINE Theme
+    // -------------
+
+    Datawrapper.Themes.SpOn = $.extend(true, {}, Datawrapper.Themes.Base, {
+
+        colors: {
+            palette: ['#a030415', '#06234c', '#014d01', '#f9830e', '#5e3310', '#4fb4c'],
+            positive: '#06234c',
+            negative: '#014d01',
+            background: '#ffffff'
+        },
+
+        lineChart: {
+            strokeWidth: 3,
+            hoverDotRadius: 4,
+            maxLabelWidth: 80
+        },
+        vpadding: 0
+
+    });
+
+}).call(this);
